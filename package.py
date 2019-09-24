f3890ec41d7e141c21cca02ccde1655ed7ba05cc
Bishoy <abdelmab@codescalers.com>
2019-09-24 10:52:06 +0200

    fix docstring of package.py

--- package.py
+++ package.py
@@ -3,7 +3,7 @@ from Jumpscale import j
 class Package(j.baseclasses.threebot_package):
     """
     to start need to run 
-    kosmos -p "j.tools.threebot_packages.get('freeflowtoken_com_production',giturl='https://github.com/freeflowtoken/www_freeflowtoken_com',branch='production')"
+    kosmos -p "j.tools.threebot_packages.get('freeflowtoken_com_stag',giturl='https://github.com/freeflowtoken/www_freeflowtoken_com',branch='master')"
     kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
     """
     def _init(self, **kwargs):
