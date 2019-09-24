0cde9033b8ec6d03ee4822bd9dd587ea57e6438b
Bishoy <abdelmab@codescalers.com>
2019-09-24 10:53:47 +0200

    add package.py should used later with threebot server

---
+++ package.py
@@ -0,0 +1,53 @@
+from Jumpscale import j
+
+class Package(j.baseclasses.threebot_package):
+    """
+    to start need to run 
+    kosmos -p "j.tools.threebot_packages.get('freeflowtoken_com_production',giturl='https://github.com/freeflowtoken/www_freeflowtoken_com',branch='production')"
+    kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
+    """
+    def _init(self, **kwargs):
+        self.branch = kwargs["package"].branch or "master"
+        self.freeflowtoken_com = "https://github.com/freeflowtoken/www_freeflowtoken_com"
+
+    def prepare(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        server = self.openresty
+        server.install(reset=True)
+        server.configure()
+        website = server.websites.get("freeflowtoken_com")
+        website.ssl = False
+        website.port = 80
+        locations = website.locations.get("freeflowtoken_com")
+        static_location = locations.locations_static.new()
+        static_location.name = "static"
+        static_location.path_url = "/"
+        path = j.clients.git.getContentPathFromURLorPath(self.freeflowtoken_com, branch=self.branch, pull=True)
+        static_location.path_location = path
+        static_location.use_jumpscale_weblibs = True
+        website.path = path
+        locations.configure()
+        website.configure()
+
+    def start(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        self.prepare()
+    def stop(self):
+        """
+        called when the 3bot stops
+        :return:
+        """
+        pass
+
+    def uninstall(self):
+        """
+        called when the package is no longer needed and will be removed from the threebot
+        :return:
+        """
+        pass
