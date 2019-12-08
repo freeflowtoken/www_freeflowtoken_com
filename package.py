a0d432b201c2e95c018cdcccdbcf66db3b917a62
Pishoy <pishoyabdo@gmail.com>
2019-12-08 14:31:25 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,51 +1,25 @@
-from Jumpscale import j
-
 class Package(j.baseclasses.threebot_package):
     """
-    to start need to run 
-    kosmos -p "j.tools.threebot_packages.get('freeflowtoken_com_stag',giturl='https://github.com/freeflowtoken/www_freeflowtoken_com',branch='master')"
-    kosmos -p "j.servers.threebot.default.start(web=True, ssl=False)"
+    kosmos -p
+    cl = j.servers.threebot.local_start_default()
+    j.threebot.packages.zerobot.packagemanager.actors.package_manager.package_add(git_url="https://github.com/freeflowtoken/www_freeflowtoken_com")
     """
+
     def _init(self, **kwargs):
         self.branch = kwargs["package"].branch or "master"
-        self.freeflowtoken_com = "https://github.com/freeflowtoken/www_freeflowtoken_com"
+        self.enertia_io = "https://github.com/freeflowtoken/www_freeflowtoken_com"
 
     def prepare(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
-        server = self.openresty
-        server.install(reset=True)
-        server.configure()
-        website = server.get_from_port(80)
+        website = self.openresty.get_from_port(443)
         locations = website.locations.get("freeflowtoken_com")
         static_location = locations.locations_static.new()
         static_location.name = "static"
         static_location.path_url = "/"
-        path = j.clients.git.getContentPathFromURLorPath(self.freeflowtoken_com, branch=self.branch, pull=True)
+        path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
         static_location.path_location = path
-        static_location.use_jumpscale_weblibs = True
-        website.path = path
+        static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
         locations.configure()
         website.configure()
 
     def start(self):
-        """
-        called when the 3bot starts
-        :return:
-        """
         self.prepare()
-    def stop(self):
-        """
-        called when the 3bot stops
-        :return:
-        """
-        pass
-
-    def uninstall(self):
-        """
-        called when the package is no longer needed and will be removed from the threebot
-        :return:
-        """
-        pass
